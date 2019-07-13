efd90565645c808823ba0f19047de6085f70aa4f
Holger H <holgheiss@gmail.com>
2019-07-13 17:25:03 +0200

    port

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const storage = require('./storage.js')
 const core = require('./core.js')
 
-const port = process.env.NODE_ENV === "production" ? 80 : 3000
+port = process.env.PORT || 80
 
 // create express server
 var app = express()
