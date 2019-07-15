620035d8a8c20cc5a6d9b12e2d0e56af599efc16
Holger H <holgheiss@gmail.com>
2019-07-15 18:17:53 +0200

    fix printer character encoding

--- machine/printer.js
+++ machine/printer.js
@@ -1,20 +1,32 @@
-const { execSync } = require('child_process');
+const SerialPort = require('serialport')
+const iconv = require('iconv-lite');
 
 const init = function() {
-  return execSync('stty -F /dev/serial0 19200')
+  port = new SerialPort('/dev/serial0', {
+    baudRate: 19200
+  })
+
+  initCodepage()
+}
+
+const initCodepage = function(number = 0x00) { // default: cp437
+  port.write([0x1b, 0x74, number])
+}
+
+const encode = function(string) {
+  return iconv.encode(string, 'cp437')
 }
 
 const print = function(text = "", style={}) {
-  return execSync(`echo -n "${text}" > /dev/serial0`)
+  return port.write(encode(text))
 }
 
 const printLn = function(text = "", style={}) {
-  return execSync(`echo "${text}" > /dev/serial0`)
+  return print(text + "\n")
 }
 
-
 const printLnLn = function(text = "", style={}) {
-  return execSync(`echo "${text}\\n\\n\\n" > /dev/serial0`)
+  return print(text + "\n\n\n")
 }
 
 module.exports = {
